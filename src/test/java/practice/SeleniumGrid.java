fe34eb226f96d5564a3f91ba6f5e895ae4e97f72
Abhishek <mishr@MISHRA>
2023-04-11 23:16:55 +0530

    committed on 11/4/2023

---
+++ src/test/java/practice/SeleniumGrid.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumGrid {
+	
+	public static void main(String[] args) throws MalformedURLException {
+		
+	//step 1: Open the application 
+		URL url=new URL("https://localhost:4444");
+		
+		//step 2: create the desired capability class
+		
+		
+		
+	}
+	
+
+}
---
+++ src/test/java/practice/WebTablePratice.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebTablePratice {
+	
+	@Test
+	public void webtablePractice()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://localhost:8888");
+		driver.findElement(By.name("user_name")).sendKeys("admin",Keys.TAB,"admin",Keys.ENTER);
+		driver.findElement(By.linkText("Organizations")).click();
+		
+		// click on all the checkbox
+		driver.findElement(By.xpath("//table[@class='lvt small']/tbody/tr[*]/td/input")).click();
+		
+		// fetch the allorg name
+		List<WebElement> allorg=driver.findElements(By.xpath("//table[@class='lvt small']/tbody/tr[*]/td[3]/a"));
+		for(WebElement org:allorg)
+		{
+			    System.out.println(org.getText());
+			
+			
+		}
+		
+		
+		//click on 10th check box
+		
+		driver.findElement(By.xpath("//table[@class='lvt small']/tbody/tr[10]/td/input")).click();
+	}
+
+}
